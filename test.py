70194310d12ddda1fe1bacdbd6d986610b455ddc
GoldinLocks <ajsim220@gmail.com>
2021-04-06 21:31:27 -0400

    commit

---
+++ test.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+from database import azure_db_utils
+
+
+# Read base_fts csv to DataFrame
+print('\nBase Features DF:') 
+base_fts_df = pd.read_csv('./data/base_features.csv', index_col=[0,1])
+print(base_fts_df.info(verbose=False))
+print(base_fts_df.tail(3))
+
+# Load DataFrame to SQL databse table 
+azure_db_utils.df_bulk_insert(
+    df=base_fts_df, 
+    table_name='TD_ML_Bot_base_Fts'
+    )
+
+# Load eng_fts database table to DataFrame 
+df = azure_db_utils.fetch_df(table_name='TD_ML_Bot_Eng_Fts')
+print('\nEngineered Features DF SQL Table:') 
+print(df.info(verbose=False))
+print(df.tail(3))
+
+# Get last date in database table
+last_date = azure_db_utils.table_last_date(table_name ='TD_ML_Bot_Eng_Fts')
+print(f'\nLast Date: \n{last_date}')
